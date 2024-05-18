271a17e0230412eb4b3b0d63d8ca2c63f40c0b57
kkkkimtaehyeon <kth1927@naver.com>
2024-05-19 01:51:31 +0900

    Initial commit

---
+++ src/test/java/com/example/grass_insurance/GrassInsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grass_insurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrassInsuranceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
